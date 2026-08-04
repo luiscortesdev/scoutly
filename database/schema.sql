a8c76c461914ac072a3a07a8369f1bd5d1daf75f
luiscortesdev <luiscortesdev@gmail.com>
2026-08-04 13:12:56 -0400

    feat(db): create user profiles schema

---
+++ database/schema.sql
@@ -0,0 +1,28 @@
+CREATE TYPE gender_type as ENUM ("men", "women")
+
+CREATE TABLE user_profiles (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(500) NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    gender gender_type NOT NULL,
+    birth_date DATE NOT NULL,
+    graduation_year VARCHAR(4) NULL,
+    address VARCHAR(500) NOT NULL,
+    city VARCHAR(500) NOT NULL,
+    state VARCHAR(500) NOT NULL,
+    zip VARCHAR(500) NOT NULL,
+    sat_reading INT NULL,
+    sat_math INT NULL,
+    sat_total INT NULL,
+    act_cumulative INT NULL,
+    gpa_unweighted NUMERIC(3,2) NULL,
+    gpa_weighted NUMERIC(3,2) NULL,
+    handicap VARCHAR(10) NULL,
+    home_course VARCHAR(255) NULL,
+    lat NUMERIC(9,6) NULL,
+    lon NUMERIC(9,6) NULL,
+    scoring_avg NUMERIC(6, 3) NULL,
+    created_at TIMESTAMPZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+)
