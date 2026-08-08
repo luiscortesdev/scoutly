bda133dd9fa78eaa7e44f7751456887fef2ccae9
luiscortesdev <luiscortesdev@gmail.com>
2026-08-08 17:29:27 -0400

    refactor(cache): update cache folder name to .cache

--- scripts/seed_colleges.py
+++ scripts/seed_colleges.py
@@ -15,8 +15,8 @@ from dotenv import load_dotenv
 COLLEGE_SCORECARD_URL = "https://collegescorecard.ed.gov/data/"
 FALLBACK_DATA_URL = "https://ed-public-download.scorecard.network/downloads/Most-Recent-Cohorts-Institution_06102026.zip" # Last Updated 8/6/26
 
-SEED_DATA_DIR = os.path.join("database", "seed_data")
-LOCAL_CSV_PATH = os.path.join(SEED_DATA_DIR, "Most-Recent-Cohorts-Institution.csv")
+CACHE_DATA_DIR = os.path.join("database", ".cache")
+LOCAL_CSV_PATH = os.path.join(CACHE_DATA_DIR, "Most-Recent-Cohorts-Institution.csv")
 TEMP_ZIP_PATH = "temp_scorecard.zip"
 
 load_dotenv()
@@ -27,7 +27,7 @@ DB_NAME = os.getenv("DB_NAME", "scoutly")
 DB_USER = os.getenv("DB_USER", "scoutly_admin")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
 
-os.makedirs(SEED_DATA_DIR, exist_ok=True)
+os.makedirs(CACHE_DATA_DIR, exist_ok=True)
 
 def get_download_url():
     try:
