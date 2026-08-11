6b06fa071ae7fe4811486ba8550a1d77bf9f26d8
luiscortesdev <luiscortesdev@gmail.com>
2026-08-11 11:49:09 -0400

    feat(scraper): create parsing function to get head coach name

--- scripts/scrape_details.py
+++ scripts/scrape_details.py
@@ -1,9 +1,9 @@
 import os
 import re
 import time
-import psycopg2
 from bs4 import BeautifulSoup
 from playwright.sync_api import sync_playwright
+import psycopg2
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -14,9 +14,32 @@ DB_NAME = os.getenv("DB_NAME", "scoutly")
 DB_USER = os.getenv("DB_USER", "scoutly_admin")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
 
+CACHE_DIR = os.path.join("database", ".cache")
+HTML_FILE = os.path.join(CACHE_DIR, "program.html")
+
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 
-# orchestration functions
+# parsing functions
+def parse_head_coach(html_content):
+    soup = BeautifulSoup(html_content, "html.parser")
+    
+    coach_label = soup.find(string=re.compile(r"Head Coach", re.IGNORECASE))
+    
+    if coach_label:
+        coach_name = coach_label.find_next()
+        if coach_name:
+            return coach_name.text.strip()
+        
+    return None
+
+# playwright configuration
+def configure_page_route(page):
+    # abort background network traffic
+    page.route("**/*sentry*", lambda route: route.abort())
+    page.route("**/*google-analytics*", lambda route: route.abort())
+    page.route("**/*hotjar*", lambda route: route.abort())
+    page.route("**/*doubleclick*", lambda route: route.abort())
+
 def scrape_program_details():
     conn = psycopg2.connect(
         host=DB_BIND_ADDRESS,
@@ -34,19 +57,28 @@ def scrape_program_details():
         browser = p.chromium.launch()
         page = browser.new_page(user_agent=USER_AGENT)
         
+        configure_page_route(page) # apply telemetry blockers
+        
         for program_id, clippd_id, name in programs:
         
             program_url = f"https://scoreboard.clippd.com/teams/{clippd_id}?season=2026"
             
             try:
-                response = page.goto(program_url, wait_until="networkidle", timeout=15000)
+                page.goto(program_url, wait_until="load", timeout=15000)
+                page.wait_for_selector("main", timeout=5000) # wait for the main content to load before parsing
                 
-                print(response.status())
                 html_content = page.content()
+                #with open(HTML_FILE, "w", encoding="utf-8") as file:
+                    #file.write(html_content)
+                
+                coach = parse_head_coach(html_content)
+                print(coach)
                 
             except Exception as e:
                 print(f"Error fetching id {clippd_id}: {e}")
-                conn.rollback()        
+                conn.rollback()   
+                
+        browser.close()
 
 if __name__ == "__main__":
     scrape_program_details()
