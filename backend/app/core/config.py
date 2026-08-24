7258fecf196a9791548b07ecf101d32e5ef3a989
luiscortesdev <luiscortesdev@gmail.com>
2026-08-23 21:25:04 -0400

    feat(config): add config settings using pydantic validation

---
+++ backend/app/core/config.py
@@ -0,0 +1,30 @@
+from typing import List
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "Scoutly API"
+    API_V1_STR: str = "/api/v1"
+    
+    POSTGRES_USER: str = "postgres"
+    POSTGRES_PASSWORD: str = "postgres"
+    POSTGRES_HOST: str = "localhost"
+    POSTGRES_PORT: str = "5432"
+    POSTGRES_DB: str = "scoutly"
+    
+    SECRET_KEY: str = "secretkey"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8  # 8 days
+    
+    # cors
+    BACKEND_CORS_ORIGINS: List[str] = ["http://localhost:3000"]
+    
+    @property
+    def async_database_url(self) -> str:
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+    
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
+    
+settings = Settings()
