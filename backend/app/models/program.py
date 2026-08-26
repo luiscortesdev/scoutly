c110f6a3f7feb6790df4a6449775cbcb9e68e8c6
luiscortesdev <luiscortesdev@gmail.com>
2026-08-25 23:42:31 -0400

    chore(deps): import needed sqlalchemy packages

--- backend/app/models/program.py
+++ backend/app/models/program.py
@@ -1,4 +1,5 @@
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import String, Integer, Numeric, DateTime, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 class Base(DeclarativeBase):
     pass
