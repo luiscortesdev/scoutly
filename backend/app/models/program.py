c47f54d9d34590275680e31a07097c358eda9333
luiscortesdev <luiscortesdev@gmail.com>
2026-08-26 15:39:23 -0400

    feat(models): create fields for program orm model

--- backend/app/models/program.py
+++ backend/app/models/program.py
@@ -1,8 +1,13 @@
-from sqlalchemy import String, Integer, Numeric, DateTime, func
+from sqlalchemy import String, Integer, Numeric, DateTime, Enum, func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 class Base(DeclarativeBase):
     pass
 
 class Program(Base):
-    pass
+    __tablename__ = "programs"
+    
+    name: Mapped[str | None] = mapped_column(String(255))
+    conference: Mapped[str | None] = mapped_column(String(255))
+    head_coach: Mapped[str | None] = mapped_column(String(255))
+    rank: Mapped[int | None] = mapped_column(Integer)
