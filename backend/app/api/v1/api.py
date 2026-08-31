e42962779a9897e5638127fe46bf94dac40eb781
luiscortesdev <luiscortesdev@gmail.com>
2026-08-30 23:58:57 -0400

    fix(api): include program router not program file

--- backend/app/api/v1/api.py
+++ backend/app/api/v1/api.py
@@ -3,4 +3,4 @@ from app.api.v1 import program
 
 api_router = APIRouter()
 
-api_router.include_router(program, prefix="/programs", tags=["programs"])
+api_router.include_router(program.router, prefix="/programs", tags=["programs"])
